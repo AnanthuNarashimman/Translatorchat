046471f3a64fe499a471a00e88f336d85f4b4181
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2024-10-09 22:21:50 +0530

    Enter key can send messages

--- chat.js
+++ chat.js
@@ -1,25 +1,14 @@
-
-
 let send = document.querySelector('.sendbtn');
 let chatarea = document.querySelector('.msgarea');
 
-function scrollToBottom() {
-    const msgArea = document.querySelector('.msgarea');
-    msgArea.scrollTop = msgArea.scrollHeight; 
-}
-
-window.onload = function() {
-    scrollToBottom();
-};
-
-send.addEventListener('click', async function() {
+// Function to handle sending message
+async function sendMessage() {
     let msgbox = document.querySelector('.msgbox');
     let msg = msgbox.value;
     if (msg) {
         // Create the user's message
         let mess = document.createElement('p');
         mess.classList.add('msg');
-        //mess.classList.add('msg');
         let sourcelang = document.querySelector('.srclang').value;
         let targetlang = document.querySelector('.trglang').value;
 
@@ -71,4 +60,21 @@ send.addEventListener('click', async function() {
             console.error('Error:', error);
         }
     }
+}
+
+
+send.addEventListener('click', sendMessage);
+
+
+let msgbox = document.querySelector('.msgbox');
+msgbox.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); 
+        sendMessage(); 
+    }
 });
+
+
+function scrollToBottom() {
+    chatarea.scrollTop = chatarea.scrollHeight;
+}
